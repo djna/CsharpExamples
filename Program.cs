043091f67075c3395fb52291ed9927016b6a6d81
David Artus <djna01@gmail.com>
2022-12-08 09:38:45 +0000

    implemented

--- Program.cs
+++ Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Collections.Generic;
 
-namespace dotnetcore
+
+namespace djna
 {
-    class Program
+    class Gutenberg
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string text = File.ReadAllText(@"Gutenberg/Wonder.txt");
+            IEnumerable<string> words = Regex.Split(text, @"\W+")
+                    .Where( w => w.Length > 2)
+                    .Select(u => u.ToLower());
+            
+            string stoptext = File.ReadAllText(@"Gutenberg/stopwords");
+            IEnumerable<string> stopwords = Regex.Split(stoptext, @"\W");
+
+            Console.WriteLine("Read {0} words of more than 2 characters  and {1} stopwords", words.Count(), stopwords.Count());
+
+            IEnumerable<string> uncommonWords = words.Where( w => ! stopwords.Contains(w) );
+
+             Console.WriteLine("{0} uncommon words", uncommonWords.Count());
+
+            var frequency = uncommonWords.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count())
+               .OrderByDescending(key => key.Value)
+               .Take(10)
+               ;
+
+            foreach (KeyValuePair<string, int> kvp in frequency) {
+               Console.WriteLine("{0} - {1}", kvp.Key, kvp.Value);
+            }
+            
+
         }
     }
 }
